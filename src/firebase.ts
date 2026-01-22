840ddd3ac37c83e4962dc7bc95875d3ad2c8a524
nickolasjoe <nickolasjoe98@gmail.com>
2026-01-22 19:33:06 -0300

    fix db

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSgO_xq9mfOvQN7bXQtDEy3ZZ1G7WQeEA",
-  authDomain: "butterywebsite.firebaseapp.com",
-  databaseURL: "https://butterywebsite-default-rtdb.firebaseio.com",
-  projectId: "butterywebsite",
-  storageBucket: "butterywebsite.appspot.com",
-  messagingSenderId: "721078494858",
-  appId: "1:721078494858:web:a71cc3e7d77ac08550ef73",
-  measurementId: "G-VQVSB69GT6",
+  apiKey: "AIzaSyD96akZfGciBle4lDQP84BYDYDKOgDwTnE",
+  authDomain: "buttery-ai.firebaseapp.com",
+  projectId: "buttery-ai",
+  storageBucket: "buttery-ai.firebasestorage.app",
+  messagingSenderId: "931125895905",
+  appId: "1:931125895905:web:f82e81bdcda8fcef504da0",
+  measurementId: "G-T13EPESL1P",
 };
 
 // Initialize Firebase
