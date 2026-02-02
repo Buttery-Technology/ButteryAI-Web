0e2f34c3b1a878f6df634f89a742554666240797
nickolasjoe <nickolasjoe98@gmail.com>
2026-02-02 17:26:26 -0300

    fix github pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
